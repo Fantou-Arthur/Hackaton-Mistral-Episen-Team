bb2b5f7b1d9d568f1493c9c49e7c571d81de4387
Eloka Michel <elokamichel@gmail.com>
2025-09-13 18:50:53 +0200

    fix:adding teams conector

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ from dotenv import load_dotenv
 from connectors import teams_connector
 
 
-PORT = int(os.getenv("PORT", "3000"))
 
-mcp = FastMCP("EPISEN_AI_TEAM_SUPPORT", port=PORT, stateless_http=True, debug=True)
+mcp = FastMCP("EPISEN_AI_TEAM_SUPPORT", port=3000, stateless_http=True, debug=True)
 
 
 
