36dbc09cbefc040d0e0a47a5347316fc7294452e
Arthur Fantou <a.fantouarthur@gmail.com>
2025-09-13 15:17:27 +0200

    +Completion of mcp tool title/description -Removed useless code from template

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ mcp = FastMCP("EPISEN_AI_TEAM_SUPPORT", port=3000, stateless_http=True, debug=Tr
 def echo(text: str = Field(description="The text to echo")) -> str:
     return text
 
+
+
+
 @mcp.tool(
     title="Great User",
     description="Great the user",
@@ -26,48 +29,33 @@ def Greet(User: str = Field(description="The User to greet")) -> str:
     return Greetings
 
 
-@mcp.tool()
+
+
+@mcp.tool(
+    title="Trello Summary",
+    description="Trello Summary tool",
+)
 async def trello_summary() -> str:
     """Résumé de l’état des tickets Trello (ToDo, Doing, Done, Blocked)."""
     return await trello_connector.sprint_summary()
 
-@mcp.tool()
+
+
+
+@mcp.tool(
+    title="Slack Summary",
+    description="Slack Summary tool",
+)
 async def slack_summary() -> str:
     """Résumé des unread messages et mentions Slack."""
     return await slack_connector.unread_mentions()
 
-@mcp.tool()
+@mcp.tool(
+    title="Google Analytics Website Summary",
+    description="Google Analytics Website Summary tool",)
 async def ga4_summary() -> str:
     """Résumé des sessions utilisateurs sur les 7 derniers jours (GA4)."""
     return await ga4_connector.sessions_report()
-
-
-
-@mcp.resource(
-    uri="greeting://{name}",
-    description="Get a personalized greeting",
-    name="Greeting Resource",
-)
-def get_greeting(
-    name: str,
-) -> str:
-    return f"Hello, {name}!"
-
-
-@mcp.prompt("")
-def greet_user(
-    name: str = Field(description="The name of the person to greet"),
-    style: str = Field(description="The style of the greeting", default="friendly"),
-) -> str:
-    """Generate a greeting prompt"""
-    styles = {
-        "friendly": "Please write a warm, friendly greeting",
-        "formal": "Please write a formal, professional greeting",
-        "casual": "Please write a casual, relaxed greeting",
-    }
-
-    return f"{styles.get(style, styles['friendly'])} for someone named {name}."
-
 
 if __name__ == "__main__":
     mcp.run(transport="streamable-http")
